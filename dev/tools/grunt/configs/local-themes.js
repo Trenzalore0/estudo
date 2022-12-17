f0de8b5900337526d7d093af99e775f0f30c5729
Kleydson Vieira <kleydson@hibrido.com.br>
2022-12-17 16:24:03 -0300

    :sparkles: feature: add my custom theme

---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+
+    default: {
+        area: 'frontend',
+        name: 'Trenzalore/Theme/Main',
+        locale: 'en_US',
+        files: [
+            'css/styles-old',
+            'css/styles'
+        ],
+        dsl: 'less'
+    }
+};
